d0fda21bd71971f8dd5c31123790c0b0a0aa0243
PeetV <peetjvermeulen@gmail.com>
2024-12-08 11:22:53 +1100

    Days 9

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,16 +1,50 @@
 fun main() {
 
     fun part1(input: List<String>): Int {
-        return input.size
+        val adjustedInput = input.first()
+        var totalScore = 0
+        var groupCount = 0
+        var depth = 0
+        var inGarbage = false
+        var i = 0
+        while (i < adjustedInput.length) {
+            when {
+                adjustedInput[i] == '!' -> i++ // Skip next character
+                inGarbage && adjustedInput[i] != '>' -> { /* Ignore garbage content */ }
+                adjustedInput[i] == '<' -> inGarbage = true
+                adjustedInput[i] == '>' -> inGarbage = false
+                adjustedInput[i] == '{' && !inGarbage -> {
+                    depth++
+                    groupCount++
+                }
+                adjustedInput[i] == '}' && !inGarbage -> {
+                    totalScore += depth
+                    depth--
+                }
+            }
+            i++
+        }
+        return totalScore
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val adjustedInput = input.first()
+        var totalScore = 0
+        var inGarbage = false
+        var i = 0
+        while (i < adjustedInput.length) {
+            when {
+                adjustedInput[i] == '!' -> i++ // Skip next character
+                inGarbage && adjustedInput[i] != '>' -> { totalScore++ }
+                adjustedInput[i] == '<' -> inGarbage = true
+                adjustedInput[i] == '>' -> inGarbage = false
+                adjustedInput[i] == '{' && !inGarbage -> { }
+                adjustedInput[i] == '}' && !inGarbage -> { }
+            }
+            i++
+        }
+        return totalScore
     }
-
-    // Test input from the `src/Day09_test.txt` file
-    val testInput = readInput("Day09_test")
-    check(part1(testInput) == 1)
 
     // Input from the `src/Day09.txt` file
     val input = readInput("Day09")
